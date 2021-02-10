296147ec3a745a0a2aea49802f722f1dcf30144e
bluemoon543 <71580930+bluemoon543@users.noreply.github.com>
2021-02-11 00:07:49 +0530

    Update gamev1.py

--- gamev1.py
+++ gamev1.py
@@ -1,33 +1,28 @@
 # paste version 1 here (you said you'll paste both the games' codes)
 # if you want to add more folders (called repls), just do it
 # i created the introductory ones so that you aren't confused
-
-################## okay lol 
-
 import turtle 
 import time 
 import random 
-slow = 0.1
+delay = 0.08
 
 # setup 
 wn = turtle.Screen()
 wn.setup(600,600)
-wn.title('feed me')
-wn.bgcolor('pink')
+wn.title('feed me ')
+wn.bgcolor('spring green')
 
-
-############ head #################
-
+############ head ##################
 head = turtle.Turtle()
 head.shape('circle')
-head.color('red')
+head.color('black')
 head.penup()
 head.goto(0,0)
 head.speed(0) #this is animation speed 
 # not the actual speed . 0 is the fastest speed 
 head.direction = 'stop' # if up , head will go up 
 
-#snake food@###
+################# snake food
 food = turtle.Turtle()
 food.shape('triangle')
 food.color('black')
@@ -35,10 +30,10 @@ food.penup()
 food.goto(0,50)
 food.speed(0) 
 
-parts = [] # used for 
+################################
+parts = []
 
-
-
+ 
 
 
 
@@ -79,6 +74,7 @@ def go_right():
     head.direction = 'right'
 
 
+
 #################################
 ### keyboard connction 
 wn.listen()
@@ -86,12 +82,16 @@ wn.onkeypress(go_up , 'w')
 wn.onkeypress(go_down , 's')
 wn.onkeypress(go_left , 'a')
 wn.onkeypress(go_right , 'd')
+##################################
+
 
    
 
 
 #@@@@@@@## main game loop 
 while True :
+
+
     wn.update()
 # ckeck for collision 
 
@@ -104,14 +104,31 @@ while True :
         # add a bodypart 
         new_part = turtle.Turtle()
         new_part.speed(0) # animation speed 
-        new_part.shape('triangle')
+        new_part.shape('circle')
         new_part.color('black')
         new_part.penup()
         parts.append(new_part)
 
+    for index in range(len(parts)-1,0,-1):
+        x = parts[index-1].xcor()
+        y = parts[index-1].ycor()
+        parts[index].goto(x,y)
+
+    
+    if len(parts) > 0 :
+        x = head.xcor()
+        y = head.ycor()
+        parts[0].goto(x,y)
 
 
-    time.sleep(slow)
+
+   
+
+
+
+
+
+    time.sleep(delay)
     move()
 
 
@@ -124,3 +141,4 @@ while True :
 
 '''turtle.done()''' # we can use this as well 
 wn.mainloop() # will keep winow open 
+ 
